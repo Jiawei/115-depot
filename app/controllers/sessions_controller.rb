086ca80eb12291581a21d9b3d6db683a0d026ea1
Jiawei <150260601@qq.com>
2012-08-06 15:31:58 +0800

    login debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,6 @@ class SessionsController < ApplicationController
 
   def create
 	  if user = User.authenticate(params[:name], params[:password])
-<<<<<<< HEAD
-=======
-	   
->>>>>>> d81c5d7fa612844e2053e8f2caa6f2c672b141c3
 		  session[:user_id] = user.id
 		  redirect_to admin_url
 		  if Cart.find_by_user_id(user.id) == nil
@@ -19,9 +15,6 @@ class SessionsController < ApplicationController
 	  else 
 		  redirect_to login_url, :alert => "Invalid user/password combination"
 	  end
-	  
-	  
-
   end
 
   def destroy
