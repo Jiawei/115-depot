59b38598fa7c5a994ee7fe2c9fed0e579d80f7db
jpbenson <18299882@qq.com>
2012-08-07 14:16:53 +0800

    a

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,10 @@
-Pony.mail({
-  :to => 'you@example.com',
-  :via => :smtp,
-  :via_options => {
-    :address              => 'smtp.gmail.com',
-    :port                 => '587',
-    :enable_starttls_auto => true,
-    :user_name            => 'web115depot',
-    :password             => 'web115depot',
-    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-  }
-})
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com'
+}
+
+ActionMailer::Base.delivery_method = :smtp
