5214f27dca26c998a27983e4151f25cc84c341c2
jpbenson <18299882@qq.com>
2012-08-07 13:58:42 +0800

    m

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['18299882@qq.com']
+  :user_name => ENV['18299882@qq.com'],
   :password => ENV['000000'],
   :domain => 'heroku.com'
 }
