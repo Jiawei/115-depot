eaceaa4325603d473824af0c6cd33f0a3b99f12c
jpbenson <18299882@qq.com>
2012-08-08 13:11:48 +0800

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
 	  if user = User.authenticate(params[:name], params[:password])
 		  session[:user_id] = user.id
-		  redirect_to admin_url
+		  redirect_to store_url
 		  if Cart.find_by_user_id(user.id) == nil
 		    Cart.create(:user_id => user.id);
       end
