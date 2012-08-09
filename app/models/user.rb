51d25927573553d52e0c44e1db243e25f68d9a08
jpbenson <18299882@qq.com>
2012-08-09 11:41:56 +0800

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ require 'digest/sha2'
 
 class User < ActiveRecord::Base
 	has_one :carts
-	USER_TYPES = ["customer", "seller", "admin"];
+	USER_TYPES = ["customer", "seller"];
   validates :name, :presence => true, :uniqueness => true
  
   validates :password, :confirmation => true
