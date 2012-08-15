1c1525260b1bb4e43539853ca07430d6c72cb5f1
jpbenson <18299882@qq.com>
2012-08-15 13:26:12 +0800

    vali

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order"]
   validates :name, :address, :email, :pay_type, :presence => true
   validates :pay_type, :inclusion => PAYMENT_TYPES
+  validates_format_of :email, :with => /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/
   
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item| 
